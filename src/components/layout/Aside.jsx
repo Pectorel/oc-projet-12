d7483d780f90d43dc31191ef0ddab124c5830d32
Pectorel <contact@devadra.com>
2023-08-23 18:25:57 +0200

    Switch from styled components to CSS Module

--- src/components/layout/Aside.jsx
+++ src/components/layout/Aside.jsx
@@ -1,54 +1,30 @@
-import styled from "styled-components";
-
-const AsideMenu = styled.aside`
-  background: #000;
-  padding: 3.75rem 1.625rem;
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  justify-content: space-between;
-  align-items: center;
-  font-weight: 500;
-`;
-
-const ActivityContainer = styled.section`
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  justify-content: center;
-  gap: 1.5rem;
-`;
-
-const ActivityBlock = styled.div`
-  height: 64px;
-  width: 64px;
-  border-radius: 6px;
-  background: ${(props) => props.theme.colors.primary} no-repeat center center;
-  background-size: 50%;
-  background-image: url(${(props) => props.img});
-  cursor: pointer;
-`;
-
-const AsideText = styled.p`
-  writing-mode: vertical-rl;
-  transform: rotate(-180deg);
-  font-size: 0.75rem;
-  letter-spacing: 0.02rem;
-  color: ${(props) => props.theme.colors.primary};
-`;
+import styles from "../../assets/style/components/aside.module.css";
 
 function Aside() {
   return (
-    <AsideMenu>
-      <ActivityContainer>
-        <ActivityBlock img={"/aside-logos/meditation.svg"}></ActivityBlock>
-        <ActivityBlock img={"/aside-logos/swim.svg"}></ActivityBlock>
-        <ActivityBlock img={"/aside-logos/bike.svg"}></ActivityBlock>
-        <ActivityBlock img={"/aside-logos/gym.svg"}></ActivityBlock>
-      </ActivityContainer>
-      <AsideText>Copyright, SportSee {new Date().getFullYear()}</AsideText>
-    </AsideMenu>
+    <aside id={styles["aside-menu"]}>
+      <section id={styles["activity-container"]}>
+        <div
+          className={styles["activity-block"]}
+          style={{ backgroundImage: `url("/aside-logos/meditation.svg")` }}
+        ></div>
+        <div
+          className={styles["activity-block"]}
+          style={{ backgroundImage: `url("/aside-logos/swim.svg")` }}
+        ></div>
+        <div
+          className={styles["activity-block"]}
+          style={{ backgroundImage: `url("/aside-logos/bike.svg")` }}
+        ></div>
+        <div
+          className={styles["activity-block"]}
+          style={{ backgroundImage: `url("/aside-logos/gym.svg")` }}
+        ></div>
+      </section>
+      <p id={styles["aside-text"]}>
+        Copyright, SportSee {new Date().getFullYear()}
+      </p>
+    </aside>
   );
 }
 
