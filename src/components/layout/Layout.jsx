ad48e31fb0c0913e70611a1a380f3860d0ac0740
Pectorel <contact@devadra.com>
2023-08-24 16:12:31 +0200

    Remove styled components dependencies

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,29 +1,7 @@
 import Header from "./Header.jsx";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import Aside from "./Aside.jsx";
-import { ThemeProvider } from "styled-components";
 import styles from "../../assets/style/components/layout.module.css";
-
-const theme = {
-  colors: {
-    primary: "#fff",
-    secondary: "#000",
-    red: {
-      base: "#ff0000",
-      dark: "#ff0101",
-      darker: "#e60000",
-    },
-    blue: {
-      base: "#4ab8ff",
-    },
-    yellow: {
-      base: "#fdcc0c",
-    },
-    pink: {
-      base: "#fd5181",
-    },
-  },
-};
 
 function Layout() {
   // Redirection to default profile
@@ -34,15 +12,13 @@ function Layout() {
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Header />
-        <div id={styles["layout"]}>
-          <Aside />
-          <main>
-            <Outlet />
-          </main>
-        </div>
-      </ThemeProvider>
+      <Header />
+      <div id={styles["layout"]}>
+        <Aside />
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </>
   );
 }
